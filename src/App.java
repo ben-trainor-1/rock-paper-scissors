473878713f2a4fc4d5b1245994f87f4a792c9e03
Ben Trainor <benjammintrain@gmail.com>
2022-11-16 09:46:16 -0600

    Functional and pretty. Need to add looping

--- src/App.java
+++ src/App.java
@@ -9,24 +9,27 @@ public class App {
 
         // Intro text
         System.out.println(
-                            "Welcome to the Rock-Paper-Scissors Dojo."
+                            "Welcome to the " + Colors.ANSI_PURPLE + "Rock-Paper-Scissors Dojo." + Colors.ANSI_RESET
+                            + Colors.ANSI_CYAN
                             + "\n0: Rock"
                             + "\n1: Paper"
                             + "\n2: Scissors"
+                            + Colors.ANSI_RESET
                             );
 
         space(1);
+        Thread.sleep(750);
 
         // Prompt for input
-        System.out.println("Player 1, what do you choose?");
+        System.out.println(Colors.ANSI_YELLOW + "Player 1" + Colors.ANSI_RESET + ", what do you choose?");
         playerOneChoice = in.nextInt();
-        System.out.println("Excellent.");
+        System.out.println(Colors.ANSI_PURPLE + "Excellent." + Colors.ANSI_RESET);
         Thread.sleep(750);
         space(1);
 
-        System.out.println("Player 2, what do you choose?");
+        System.out.println(Colors.ANSI_YELLOW + "Player 2" + Colors.ANSI_RESET + ", what do you choose?");
         playerTwoChoice = in.nextInt();
-        System.out.println("Excellent.");
+        System.out.println(Colors.ANSI_PURPLE + "Excellent." + Colors.ANSI_RESET);
         Thread.sleep(750);
         space(1);
 
@@ -37,13 +40,13 @@ public class App {
         Thread.sleep(500);
         System.out.print("scissors, ");
         Thread.sleep(500);
-        System.out.print("shoot!");
+        System.out.print(Colors.ANSI_RED + "shoot!" + Colors.ANSI_RESET);
         Thread.sleep(1000);
         space(2);
 
-        System.out.println("Player one chose " + rpsString(playerOneChoice));
+        System.out.println("Player 1 chose " + rpsString(playerOneChoice));
         space(1);
-        System.out.println("Player two chose " + rpsString(playerTwoChoice));
+        System.out.println("Player 2 chose " + rpsString(playerTwoChoice));
         space(1);
         
         // Identify the winner!
@@ -78,18 +81,18 @@ public class App {
         // Player 1 wins
         else if (
                 (playerOneChoice == 0 && playerTwoChoice == 2) ||
-                (playerOneChoice == 1 && playerTwoChoice == 1) ||
-                (playerOneChoice == 2 && playerTwoChoice == 0)
+                (playerOneChoice == 1 && playerTwoChoice == 0) ||
+                (playerOneChoice == 2 && playerTwoChoice == 1)
         ) {
-            System.out.println("Player one wins!");
+            System.out.println(Colors.ANSI_GREEN + "Player 1 wins!" + Colors.ANSI_RESET);
         }
         // Player 2 wins
         else if (
                 (playerTwoChoice == 0 && playerOneChoice == 2) ||
-                (playerTwoChoice == 1 && playerOneChoice == 1) ||
-                (playerTwoChoice == 2 && playerOneChoice == 0)
+                (playerTwoChoice == 1 && playerOneChoice == 0) ||
+                (playerTwoChoice == 2 && playerOneChoice == 1)
         ) {
-            System.out.println("Player two wins!");
+            System.out.println(Colors.ANSI_GREEN + "Player 2 wins!" + Colors.ANSI_RESET);
         }
         
     }
