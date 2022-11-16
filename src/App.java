165952617054d8fa9571ed6e6b42b119f70dd2c1
Ben Trainor <benjammintrain@gmail.com>
2022-11-16 09:37:04 -0600

    Basic RPS working with artificial pauses

--- src/App.java
+++ src/App.java
@@ -7,17 +7,30 @@ public class App {
         Scanner in = new Scanner(System.in);
         int playerOneChoice, playerTwoChoice;
 
+        // Intro text
+        System.out.println(
+                            "Welcome to the Rock-Paper-Scissors Dojo."
+                            + "\n0: Rock"
+                            + "\n1: Paper"
+                            + "\n2: Scissors"
+                            );
+
+        space(1);
+
+        // Prompt for input
         System.out.println("Player 1, what do you choose?");
         playerOneChoice = in.nextInt();
         System.out.println("Excellent.");
         Thread.sleep(750);
+        space(1);
 
         System.out.println("Player 2, what do you choose?");
         playerTwoChoice = in.nextInt();
         System.out.println("Excellent.");
         Thread.sleep(750);
+        space(1);
 
-        // TODO: Count down, reveal choices
+        // Count down, reveal choices
         System.out.print("Rock, ");
         Thread.sleep(500);
         System.out.print("paper, ");
@@ -26,18 +39,23 @@ public class App {
         Thread.sleep(500);
         System.out.print("shoot!");
         Thread.sleep(1000);
-        System.out.println();
+        space(2);
 
-        System.out.print("Player one chose " + compareRPS(playerOneChoice));
-        System.out.print("Player one chose " + compareRPS(playerTwoChoice));
+        System.out.println("Player one chose " + rpsString(playerOneChoice));
+        space(1);
+        System.out.println("Player two chose " + rpsString(playerTwoChoice));
+        space(1);
+        
+        // Identify the winner!
+        Thread.sleep(1000);
+        rpsCompare(playerOneChoice, playerTwoChoice);
 
-        // TODO: Identify the winner!
 
         in.close();
 
     }
 
-    public static String compareRPS(int rpsChoice) {
+    public static String rpsString(int rpsChoice) {
 
         switch (rpsChoice) {
             case 0: 
@@ -48,6 +66,37 @@ public class App {
                 return "scissors";
             default: 
                 return null;
+        }
+    }
+
+    public static void rpsCompare(int playerOneChoice, int playerTwoChoice) {
+        
+        // Draw
+        if (playerOneChoice == playerTwoChoice) {
+
+        }
+        // Player 1 wins
+        else if (
+                (playerOneChoice == 0 && playerTwoChoice == 2) ||
+                (playerOneChoice == 1 && playerTwoChoice == 1) ||
+                (playerOneChoice == 2 && playerTwoChoice == 0)
+        ) {
+            System.out.println("Player one wins!");
+        }
+        // Player 2 wins
+        else if (
+                (playerTwoChoice == 0 && playerOneChoice == 2) ||
+                (playerTwoChoice == 1 && playerOneChoice == 1) ||
+                (playerTwoChoice == 2 && playerOneChoice == 0)
+        ) {
+            System.out.println("Player two wins!");
+        }
+        
+    }
+
+    public static void space(int numSpaces) {
+        for (int i = numSpaces; i > 0; i--) {
+            System.out.print("\n");
         }
     }
 
