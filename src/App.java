cd7145a802aca2a02f5b8256fa938337f3574d06
Ben Trainor <benjammintrain@gmail.com>
2022-11-16 12:15:40 -0600

    Looping and using strings, not integers

--- src/App.java
+++ src/App.java
@@ -3,10 +3,6 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        // Choose rock paper or scissors and store in integers
-        Scanner in = new Scanner(System.in);
-        int playerOneChoice, playerTwoChoice;
-
         // Intro text
         System.out.println(
                             "Welcome to the " + Colors.ANSI_PURPLE + "Rock-Paper-Scissors Dojo." + Colors.ANSI_RESET
@@ -19,43 +15,69 @@ public class App {
 
         space(1);
         Thread.sleep(750);
-
-        // Prompt for input
-        System.out.println(Colors.ANSI_YELLOW + "Player 1" + Colors.ANSI_RESET + ", what do you choose?");
-        playerOneChoice = in.nextInt();
-        System.out.println(Colors.ANSI_PURPLE + "Excellent." + Colors.ANSI_RESET);
-        Thread.sleep(750);
-        space(1);
-
-        System.out.println(Colors.ANSI_YELLOW + "Player 2" + Colors.ANSI_RESET + ", what do you choose?");
-        playerTwoChoice = in.nextInt();
-        System.out.println(Colors.ANSI_PURPLE + "Excellent." + Colors.ANSI_RESET);
-        Thread.sleep(750);
-        space(1);
-
-        // Count down, reveal choices
-        System.out.print("Rock, ");
-        Thread.sleep(500);
-        System.out.print("paper, ");
-        Thread.sleep(500);
-        System.out.print("scissors, ");
-        Thread.sleep(500);
-        System.out.print(Colors.ANSI_RED + "shoot!" + Colors.ANSI_RESET);
-        Thread.sleep(1000);
-        space(2);
-
-        System.out.println("Player 1 chose " + rpsString(playerOneChoice));
-        space(1);
-        System.out.println("Player 2 chose " + rpsString(playerTwoChoice));
-        space(1);
         
-        // Identify the winner!
-        Thread.sleep(1000);
-        rpsCompare(playerOneChoice, playerTwoChoice);
+        Scanner in = new Scanner(System.in);
+        String playerOneChoice, playerTwoChoice;
+
+        // Game loop
+        do {
+
+            System.out.println("Would you like to play a game?");
+            space(1);
+            
+            if (in.nextLine().equals("yes")) {
+
+                space(1);
+
+                // Choose rock paper or scissors and store in integers
+                System.out.println(Colors.ANSI_YELLOW + "Player 1" + Colors.ANSI_RESET + ", what do you choose?");
+                playerOneChoice = in.nextLine();
+                System.out.println(Colors.ANSI_PURPLE + "Excellent." + Colors.ANSI_RESET);
+                Thread.sleep(750);
+                space(1);
+    
+                System.out.println(Colors.ANSI_YELLOW + "Player 2" + Colors.ANSI_RESET + ", what do you choose?");
+                playerTwoChoice = in.nextLine();
+                System.out.println(Colors.ANSI_PURPLE + "Excellent." + Colors.ANSI_RESET);
+                Thread.sleep(750);
+                space(1);
+    
+                // Count down, reveal choices
+                System.out.print("Rock, ");
+                Thread.sleep(500);
+                System.out.print("paper, ");
+                Thread.sleep(500);
+                System.out.print("scissors, ");
+                Thread.sleep(500);
+                System.out.print(Colors.ANSI_RED + "shoot!" + Colors.ANSI_RESET);
+                Thread.sleep(1000);
+                space(2);
+    
+                System.out.println("Player 1 chose " + playerOneChoice);
+                space(1);
+                System.out.println("Player 2 chose " + playerTwoChoice);
+                space(1);
+                
+                // Identify the winner!
+                Thread.sleep(1000);
+                rpsCompareString(playerOneChoice, playerTwoChoice);
+
+            }
+
+            else {
+                System.out.println("See you later!");
+                break;
+            }
+
+        } while (true);
 
 
         in.close();
 
+    }
+
+    public static void rpsGameLoop() {
+        
     }
 
     public static String rpsString(int rpsChoice) {
@@ -72,7 +94,8 @@ public class App {
         }
     }
 
-    public static void rpsCompare(int playerOneChoice, int playerTwoChoice) {
+    // Compare choices of players using integers
+    public static void rpsCompareInt(int playerOneChoice, int playerTwoChoice) {
         
         // Draw
         if (playerOneChoice == playerTwoChoice) {
@@ -96,6 +119,33 @@ public class App {
         }
         
     }
+
+    // Compare choices of players using Strings
+    public static void rpsCompareString(String playerOneChoice, String playerTwoChoice) {
+        
+        // Draw
+        if (playerOneChoice == playerTwoChoice) {
+
+        }
+        // Player 1 wins
+        else if (
+                (playerOneChoice.equals("rock") && playerTwoChoice.equals("scissors")) ||
+                (playerOneChoice.equals("paper") && playerTwoChoice.equals("rock")) ||
+                (playerOneChoice.equals("scissors") && playerTwoChoice.equals("paper"))
+        ) {
+            System.out.println(Colors.ANSI_GREEN + "Player 1 wins!" + Colors.ANSI_RESET);
+        }
+        // Player 2 wins
+        else if (
+                (playerTwoChoice.equals("rock") && playerOneChoice.equals("scissors")) ||
+                (playerTwoChoice.equals("paper") && playerOneChoice.equals("rock")) ||
+                (playerTwoChoice.equals("scissors") && playerOneChoice.equals("paper"))
+        ) {
+            System.out.println(Colors.ANSI_GREEN + "Player 2 wins!" + Colors.ANSI_RESET);
+        }
+        
+    }
+
 
     public static void space(int numSpaces) {
         for (int i = numSpaces; i > 0; i--) {
